83475fb512c4e7f7375df21ad32a9197826f18b4
dinaaa25 <dina.lazorenko@gmail.com>
2024-03-24 12:50:20 +0100

    initial commit

---
+++ temperature-service/src/test/java/com/greenhouse/springbootgreenhouse/SpringBootGreenhouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenhouse.springbootgreenhouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGreenhouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
